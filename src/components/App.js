7057a74aea4b122297fd8d23786b57d89480a4fc
Omkar Baburao Kamble <121740559+okamble605@users.noreply.github.com>
2025-03-08 18:59:30 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,54 @@
+import React, { useEffect } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { fetchBooks, sortBooks } from './redux/actions';
 
-import React from "react";
-import './../styles/App.css';
+const BooksList = () => {
+    const dispatch = useDispatch();
+    const { books, loading, error } = useSelector((state) => state.books);
 
-const App = () => {
-  return (
-    <div>
-        {/* Do not remove the main div */}
-    </div>
-  )
-}
+    useEffect(() => {
+        dispatch(fetchBooks());
+    }, [dispatch]);
 
-export default App
+    const handleSortChange = (e) => {
+        const [criteria, order] = e.target.value.split('-');
+        dispatch(sortBooks(criteria, order));
+    };
+
+    if (loading) return <p>Loading...</p>;
+    if (error) return <p>Error: {error}</p>;
+
+    return (
+        <div>
+            <h1>Book List</h1>
+            <select onChange={handleSortChange}>
+                <option value="title-asc">Title (A-Z)</option>
+                <option value="title-desc">Title (Z-A)</option>
+                <option value="author-asc">Author (A-Z)</option>
+                <option value="author-desc">Author (Z-A)</option>
+                <option value="publisher-asc">Publisher (A-Z)</option>
+                <option value="publisher-desc">Publisher (Z-A)</option>
+            </select>
+            <table>
+                <thead>
+                    <tr>
+                        <th>Title</th>
+                        <th>Author</th>
+                        <th>Publisher</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    {books.map((book) => (
+                        <tr key={book.primary_isbn13}>
+                            <td>{book.title}</td>
+                            <td>{book.author}</td>
+                            <td>{book.publisher}</td>
+                        </tr>
+                    ))}
+                </tbody>
+            </table>
+        </div>
+    );
+};
+
+export default BooksList;
